1c025a8bafb639727a77ad6ab3ff1c39ad1232d7
Robin2000 <29698868@qq.com>
2016-01-21 23:15:45 +0800

    通用队列改为boost::lockfree

--- src/shared/LockedQueue.h
+++ src/shared/LockedQueue.h
@@ -19,22 +19,24 @@
 #ifndef LOCKEDQUEUE_H
 #define LOCKEDQUEUE_H
 
-#include <deque>
+//#include <deque>
 #include <assert.h>
-#include <mutex>
+//#include <mutex>
 #include "Errors.h"
 #include "Policies/Lock.h"
+#include <boost/lockfree/queue.hpp>
 
 namespace ACE_Based
 {
-    template <class T, typename StorageType = std::deque<T> >
+    //template <class T, typename StorageType = std::deque<T> >
+	template <class T, typename StorageType = boost::lockfree::queue<T, boost::lockfree::fixed_sized<false>> >
     class LockedQueue
     {
             //! Lock access to the queue.
-            std::mutex _lock;
+            //std::mutex _lock;不加锁
 
             //! Storage backing the queue.
-            StorageType _queue;
+		StorageType _queue;
 
             //! Cancellation flag.
             /*volatile*/ bool _canceled;
@@ -43,7 +45,7 @@ namespace ACE_Based
 
             //! Create a LockedQueue.
             LockedQueue()
-                : _canceled(false)
+				:_queue(0),_canceled(false)
             {
             }
 
@@ -55,20 +57,22 @@ namespace ACE_Based
             //! Adds an item to the queue.
             void add(const T& item)
             {
-                std::lock_guard<decltype(_lock)> g(_lock);
-                _queue.push_back(item);
+                //std::lock_guard<decltype(_lock)> g(_lock);不加锁
+                //_queue.push_back(item);
+				_queue.push(item);
             }
 
             //! Gets the next result in the queue, if any.
             bool next(T& result)
             {
-                GUARD_RETURN(_lock, false);
+                //GUARD_RETURN(_lock, false);不加锁
 
                 if (_queue.empty())
                     return false;
 
-                result = _queue.front();
-                _queue.pop_front();
+                //result = _queue.front();
+                //_queue.pop_front();
+				_queue.pop(result);
 
                 return true;
             }
@@ -76,23 +80,24 @@ namespace ACE_Based
             template<class Checker>
             bool next(T& result, Checker& check)
             {
-                GUARD_RETURN(_lock, false);
+                //GUARD_RETURN(_lock, false);不加锁
 
                 if (_queue.empty())
                     return false;
 
-                result = _queue.front();
+                //result = _queue.front();
+				_queue.pop(result);
                 if (!check.Process(result))
                     return false;
 
-                _queue.pop_front();
+                //_queue.pop_front();
                 return true;
             }
 
             //! Peeks at the top of the queue. Remember to unlock after use.
             T& peek()
             {
-                lock();
+                //lock();不加锁
 
                 T& result = _queue.front();
 
@@ -102,33 +107,33 @@ namespace ACE_Based
             //! Cancels the queue.
             void cancel()
             {
-                std::lock_guard<decltype(_lock)> g(_lock);
+                //std::lock_guard<decltype(_lock)> g(_lock);不加锁
                 _canceled = true;
             }
 
             //! Checks if the queue is cancelled.
             bool cancelled()
             {
-                std::lock_guard<decltype(_lock)> g(_lock);
+                //std::lock_guard<decltype(_lock)> g(_lock);不加锁
                 return _canceled;
             }
 
             //! Locks the queue for access.
             void lock()
             {
-                _lock.lock();
+                //_lock.lock();不加锁
             }
 
             //! Unlocks the queue.
             void unlock()
             {
-                _lock.unlock();
+                //_lock.unlock();不加锁
             }
 
             ///! Checks if we're empty or not with locks held
             bool empty()
             {
-                std::lock_guard<decltype(_lock)> g(_lock);
+                //std::lock_guard<decltype(_lock)> g(_lock);不加锁
                 return _queue.empty();
             }
     };
