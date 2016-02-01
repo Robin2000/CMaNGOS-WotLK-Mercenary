779d02bd6be4d1a999199e3d6085b56f7801b10a
Robin2000 <29698868@qq.com>
2016-02-01 09:46:01 +0800

    修改菜单

--- src/myscript/pr_hearthstone.cpp
+++ src/myscript/pr_hearthstone.cpp
@@ -19,19 +19,21 @@ EndScriptData */
 
 
 /*
-
+insert into npc_text(ID,text0_0)values(16777212,'设置返回点成功,原力与你同在！');
 insert into npc_text(ID,text0_0)values(16777213,'在线即可累积原力。');
 insert into npc_text(ID,text0_0)values(16777214,'原力与你同在！更多信息请移步交流论坛：http://mangos.mybbs.us。');
 
 insert into custom_texts(entry, content_default)values(-2800173,'当前原力值：%d');
-insert into custom_texts(entry, content_default)values(-2800174,'设置回城地点。（-3原力）');
-insert into custom_texts(entry, content_default)values(-2800175,'返回回城地点。（-2原力）');
-insert into custom_texts(entry, content_default)values(-2800176,'返回炉石绑定地点。');
+insert into custom_texts(entry, content_default)values(-2800174,'设置返回点。（-3原力）');
+insert into custom_texts(entry, content_default)values(-2800175,'前往返回点。（-2原力）');
+insert into custom_texts(entry, content_default)values(-2800176,'回家。');
 insert into custom_texts(entry, content_default)values(-2800177,'系统提示：原力不足，无法这样做。');
-insert into custom_texts(entry, content_default)values(-2800178,'系统提示：还没有设置回城点。');
-insert into custom_texts(entry, content_default)values(-2800179,'系统提示：设置回城点成功。');
-insert into custom_texts(entry, content_default)values(-2800180,'系统提示：无法在副本中设置回城点。');
+insert into custom_texts(entry, content_default)values(-2800178,'系统提示：还没有设置返回点。');
+insert into custom_texts(entry, content_default)values(-2800179,'系统提示：设置返回点成功。');
+insert into custom_texts(entry, content_default)values(-2800180,'系统提示：禁止在副本中设置返回点。');
 insert into custom_texts(entry, content_default)values(-2800181,'返回上级菜单。');
+
+insert into gossip_menu(entry,text_id) values(65535,16777213);
 */
 bool hearthstone_click(Player* pPlayer, Item* pItem, SpellCastTargets const& /*scTargets*/)
 {
@@ -40,11 +42,11 @@ bool hearthstone_click(Player* pPlayer, Item* pItem, SpellCastTargets const& /*s
 	char * title=new char[1024];
 	sprintf(title, pPlayer->GetMangosString(-2800173), pPlayer->GetGamePointMgr().getGamePoint()); // 当前原力值：%d
 	
-	pPlayer->PrepareGossipMenu(pPlayer, 100000);//100000是不存在的menuid，数据库中目前最大为50101，
+	pPlayer->PrepareGossipMenu(pPlayer, 65535);//65535是不存在的menuid，数据库中目前最大为50101，
 	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, title, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);  // 当前原力值：%d
-	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, -2800174, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);//设置回城地点。（-3原力）
-	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, -2800175, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);//返回回城地点。（-2原力）
-	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, -2800176, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);//返回炉石绑定地点。
+	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, -2800174, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);//设置返回点。（-3原力）
+	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, -2800175, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);//前往返回点。（-2原力）
+	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, -2800176, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);//回家。
 	pPlayer->SEND_GOSSIP_MENU(16777213, pItem->GetObjectGuid()); //在线即可累积原力。
 
 	return true;
@@ -56,19 +58,23 @@ bool hearthstone_menu_click(Player* pPlayer, Item* pItem, uint32 /*uiSender*/, u
 		ChatHandler(pPlayer).SendSysMessage(23);//23 系统提示：在战斗中无法这样做。
 	else if (uiAction == GOSSIP_ACTION_INFO_DEF + 1) //当前原力值：%d
 	{
-		pPlayer->PrepareGossipMenu(pPlayer, 100000);//100000是不存在的menuid，数据库中目前最大为50101，
+		pPlayer->PrepareGossipMenu(pPlayer, 65535);//65535已添加到数据库
 		//pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, -2800181, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);  // 返回上级菜单。
 		pPlayer->SEND_GOSSIP_MENU(16777214, pItem->GetObjectGuid()); //'原力与你同在！更多信息请移步交流论坛：http://mangos.mybbs.us。'
-	}else if (uiAction == GOSSIP_ACTION_INFO_DEF + 2){//设置回城点。（-3原力）
-		if(pPlayer->GetGamePointMgr().setReturnPoint(3))
-			pPlayer->PlayerTalkClass->SendPointOfInterest(pPlayer->GetPositionX(), pPlayer->GetPositionY(), 7, 583, 0, "test");
-		//pPlayer->PlayerTalkClass->SendPointOfInterest(target->GetPositionX(), target->GetPositionY(), Poi_Icon(icon), flags, 30, "Test POI");
-
+		return true;
+	}else if (uiAction == GOSSIP_ACTION_INFO_DEF + 2){//设置返回点。（-3原力）
+		pPlayer->GetGamePointMgr().setReturnPoint(3);
+		if (pPlayer->GetGamePointMgr().setReturnPoint(3))
+		{
+			//pPlayer->PlayerTalkClass->SendPointOfInterest(pPlayer->GetPositionX(), pPlayer->GetPositionY(), 7, 39, 0, "ReturnPoint");
+			//pPlayer->SEND_GOSSIP_MENU(16777212, pItem->GetObjectGuid());//设置返回点成功,原力与你同在！
+		}
+		//return true;
 	}
-	else if (uiAction == GOSSIP_ACTION_INFO_DEF +3)//返回回城地点。（-2原力）
+	else if (uiAction == GOSSIP_ACTION_INFO_DEF +3)//前往返回地点。（-2原力）
 	{
 		pPlayer->GetGamePointMgr().useReturnPoint(2);
-	}else if (uiAction == GOSSIP_ACTION_INFO_DEF + 4)//传送我到炉石绑定地点。
+	}else if (uiAction == GOSSIP_ACTION_INFO_DEF + 4)//回家。
 	{
 		pPlayer->TeleportToHomebind();
 	}
