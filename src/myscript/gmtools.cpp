edb0bc186c4e33105ca384bc374a79977c42fe0f
Robin2000 <29698868@qq.com>
2016-03-18 06:59:42 +0800

    gm工具清理行李

--- src/myscript/gmtools.cpp
+++ src/myscript/gmtools.cpp
@@ -12,6 +12,7 @@ void hearthstone_prepare_gmtools(Player* pPlayer, Item* pItem, uint32 uiAction){
 	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "modify aspeed 10", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 104);
 	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "modify aspeed 1", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 105);
 	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "gm fly on/off", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 106);
+	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "clear bag", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 108);
 	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, -2800181, GOSSIP_SENDER_MAIN, 999);//返回主菜单
 	pPlayer->SEND_GOSSIP_MENU(16777210, pItem->GetObjectGuid()); //利用原力直达游戏目标。
 }
@@ -21,6 +22,38 @@ void modifySpeed(Player* pPlayer, float modSpeed){
 	pPlayer->UpdateSpeed(MOVE_SWIM, true, modSpeed);
 	// chr->UpdateSpeed(MOVE_TURN,   true, modSpeed);
 	pPlayer->UpdateSpeed(MOVE_FLIGHT, true, modSpeed);
+}
+void removeAllItem(Player* player){
+	for (int i = INVENTORY_SLOT_ITEM_START; i < INVENTORY_SLOT_ITEM_END; ++i)
+	if (Item* pItem = player->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+		player->RemoveItem(INVENTORY_SLOT_BAG_0,i, false);
+
+	for (int i = INVENTORY_SLOT_BAG_START; i < INVENTORY_SLOT_BAG_END; ++i)
+	if (Bag* pBag = (Bag*)player->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+	for (uint32 j = 0; j < pBag->GetBagSize(); ++j)
+	if (Item* pItem = pBag->GetItemByPos(j))
+		player->RemoveItem(INVENTORY_SLOT_BAG_0, j, false);
+
+	for (int i = BANK_SLOT_ITEM_START; i < BANK_SLOT_ITEM_END; ++i)
+	if (Item* pItem = player->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+		player->RemoveItem(INVENTORY_SLOT_BAG_0, i, false);
+
+	for (int i = BANK_SLOT_BAG_START; i < BANK_SLOT_BAG_END; ++i)
+	if (Bag* pBag = (Bag*)player->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+	for (uint32 j = 0; j < pBag->GetBagSize(); ++j)
+	if (Item* pItem = pBag->GetItemByPos(j))
+		player->RemoveItem(INVENTORY_SLOT_BAG_0, j, false);
+
+
+	int32 count=1;
+	uint32 noSpaceForCount = 0;
+	ItemPosCountVec dest;
+	uint8 msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, 6948, 1, &noSpaceForCount);
+	if (msg != EQUIP_ERR_OK)                                // convert to possible store amount
+		count -= noSpaceForCount;
+
+	Item* item = player->StoreNewItem(dest, 6948, true, Item::GenerateItemRandomPropertyId(6948));
+
 }
 void hearthstone_click_gmtools(Player* pPlayer, Item* pItem, uint32 uiAction){
 	switch (uiAction )
@@ -48,5 +81,8 @@ void hearthstone_click_gmtools(Player* pPlayer, Item* pItem, uint32 uiAction){
 			break;
 		case GOSSIP_ACTION_INFO_DEF + 107:
 			pPlayer->context.calculateParallelZone_quest_npcgo_all_map();
+			break;
+		case GOSSIP_ACTION_INFO_DEF + 108:
+			removeAllItem(pPlayer);
 	}
 }
