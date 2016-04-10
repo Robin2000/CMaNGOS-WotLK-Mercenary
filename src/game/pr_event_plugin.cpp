aaae4d0b6918ed630d127980df15aec3adbbfa4d
Robin2000 <29698868@qq.com>
2016-04-10 19:16:36 +0800

    调整事件触发后雇佣兵初始化的代码

--- src/game/pr_event_plugin.cpp
+++ src/game/pr_event_plugin.cpp
@@ -18,14 +18,20 @@ public:
 			//player->PetSpellInitialize();
 		if (Pet * pet = player->GetPet())
 		{
+			
 			player->PetSpellInitialize();
 			if (pet->isMercenary())
 			{
-				if (Mercenary * mercenary = player->context.GetMercenary())//首先清零，再通过AI的Update更新
+				if (Mercenary * mercenary = player->context.GetMercenary())
 				{
-					pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID, mercenary->gearContainer[SLOT_MAIN_HAND].itemid);
-					pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, mercenary->gearContainer[SLOT_OFF_HAND].itemid);
-					pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2, mercenary->gearContainer[SLOT_RANGED].itemid);
+					if (mercenary->isRangedAttack())
+						pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2, mercenary->gearContainer[SLOT_RANGED].itemid);
+					else
+					{
+						pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID, mercenary->gearContainer[SLOT_MAIN_HAND].itemid);
+						pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, mercenary->gearContainer[SLOT_OFF_HAND].itemid);
+					}
+					
 				}
 			}
 		}
@@ -40,6 +46,12 @@ void PrEventPlugin::sendEvent(PrEvent e){
 			//player->context.addDelayedAction(new DelayedHandleLogin(5000, player));//并不能解决问题，任务从跟踪列表去掉，但在日志中存在
 			if (player->context.displayid>0)
 				player->SetDisplayId(player->context.displayid);//易容
+			else
+			{
+				if (Pet * pet = player->GetPet())
+				if (pet->isMercenary())
+					player->context.GetMercenary()->SendMirrorImagePacket(pet);
+			}
 			break;
 		case P_LOGOUT_EVENT:
 			player->context.ClearMercenary();//客户端清理
@@ -49,7 +61,7 @@ void PrEventPlugin::sendEvent(PrEvent e){
 			{
 				if (pet->isMercenary())
 				{
-					if (Mercenary * mercenary = player->context.GetMercenary())//首先清零，再通过AI的Update更新
+					if (Mercenary * mercenary = player->context.GetMercenary())
 					{
 						pet->SetFlag(UNIT_FIELD_FLAGS_2, UNIT_FLAG2_CLONED);
 						pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2, 0);
