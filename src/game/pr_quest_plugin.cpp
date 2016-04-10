867cbf156d12cd0081f5a531f9f61744fa7f0b5d
Robin2000 <29698868@qq.com>
2016-04-10 20:23:52 +0800

    拦截初始任务，实现将炉石放到工具栏

--- src/game/pr_quest_plugin.cpp
+++ src/game/pr_quest_plugin.cpp
@@ -1,24 +1,31 @@
 #include "pr_quest_plugin.h"
 
 void PrQuestPlugin::AddQuest(uint32 questid){
-	if (questid == 3913){/*墓地相见->寻找墓碑*/
+	switch (questid)
+	{
+	case 3913:/*墓地相见->寻找墓碑*/
 		player->ResurrectPlayer(true, false);
-
 		player->SetSpeedRate(MOVE_WALK, 1.0f, true);
 		player->SetSpeedRate(MOVE_RUN, 1.0f, true);
 		player->SetSpeedRate(MOVE_FLIGHT, 1.0f, true);
-
 		player->DestroyItemCount(11243, 1, true, false, false);//删除物品灵视药剂，为了防止玩家被灵魂治疗师复活，药剂在这里才删除。
+		break;
 	}
 }
 void PrQuestPlugin::CompleteQuest(uint32 questid){
+
 
 }
 void PrQuestPlugin::IncompleteQuest(uint32 questid){
 
 }
 void PrQuestPlugin::rewardQuest(uint32 questid){
-
+	switch (questid)
+	{
+		case 99999://炉石传说，第一个任务
+			player->addActionButton(0, 6, 6948, 128);//0是基础工具条，6工具条位置，6948炉石，128是item
+			break;
+	}
 }
 void PrQuestPlugin::FailQuest(uint32 questid){
 
